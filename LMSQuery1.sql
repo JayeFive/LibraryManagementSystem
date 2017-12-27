df2610550d8a341b441288e1e8db78a72eb650ba
Johnny Olson <john.olson3@gmail.com>
2017-12-26 18:38:48 -0800

    Create all tables. First iteration

--- LMSQuery1.sql
+++ LMSQuery1.sql
@@ -1,13 +1,53 @@
 CREATE SCHEMA Library;
 GO
 
+-- Create Tables --
 CREATE TABLE Library.Book (
-	BookID INT PRIMARY KEY NOT NULL IDENTITY (1,1),
+	BookID INT PRIMARY KEY NOT NULL,
 	BookTitle VARCHAR(100) NOT NULL,
 	PublisherName VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Library.BookAuthors (
-	
+CREATE TABLE Library.LibraryBranch (
+	BranchID INT PRIMARY KEY NOT NULL,
+	BranchName VARCHAR(100) NOT NULL,
+	Address VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE Library.Borrower (
+	CardNo INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(100) NOT NULL,
+	Address VARCHAR(100) NOT NULL,
+	Phone VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE Library.BookAuthors (
+	BookID INT NOT NULL,
+	AuthorName VARCHAR(100) NOT NULL,
+	CONSTRAINT fk_bookid FOREIGN KEY (BookID) REFERENCES Library.Book (BookID) 
+);
+
+CREATE TABLE Library.Publisher (
+	Name VARCHAR(100) PRIMARY KEY NOT NULL,
+	Address VARCHAR(100) NOT NULL,
+	Phone VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE Library.BookLoans (
+	BookID INT NOT NULL,
+	BranchID INT NOT NULL,
+	CardNo INT NOT NULL,
+	DateOut DATE NOT NULL,
+	DueDate DATE NOT NULL,
+	CONSTRAINT fk_bookid FOREIGN KEY (BookID) REFERENCES Library.Book (BookID),
+	CONSTRAINT fk_cardno FOREIGN KEY (CardNo) REFERENCES Library.Borrower (CardNo)
+);
+
+CREATE TABLE Library.BookCopies (
+	BookID INT NOT NULL,
+	BranchID INT NOT NULL,
+	NoOfCopies INT NOT NULL,
+	CONSTRAINT fk_bookid FOREIGN KEY (BookID) REFERENCES Library.Book (BookID)
+);	
+
 
