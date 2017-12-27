1cf1f45ed67fbd2cd1d7ee2da43c61f994d0c2b1
Johnny Olson <john.olson3@gmail.com>
2017-12-27 12:03:19 -0800

    Create new Query for the very WET style of inserting data into the tables

---
+++ InsertionQuery.sql
@@ -0,0 +1,50 @@
+USE LMS;
+
+
+
+-- Insert records into Library.Book --
+
+EXEC [Library].[uspInsertLibraryBook] 'The Lost Tribe', 'West 26th Street Press';
+EXEC [Library].[uspInsertLibraryBook] 'The 7 Habits of Highly Effective People', 'Free Press';
+EXEC [Library].[uspInsertLibraryBook] 'The Power of Now: A Guide to Spiritual Enlightenment', 'New World Library';
+EXEC [Library].[uspInsertLibraryBook] 'Ready Player One', 'Random House';
+EXEC [Library].[uspInsertLibraryBook] 'The Alchemist', 'Harper Collins';
+EXEC [Library].[uspInsertLibraryBook] 'The Family', 'Regan Books';
+EXEC [Library].[uspInsertLibraryBook] 'Mastery', 'Plume';
+EXEC [Library].[uspInsertLibraryBook] 'Infinite Jest', 'Little, Brown and Company';
+EXEC [Library].[uspInsertLibraryBook] 'Ender''s Game', 'Tor Books';
+EXEC [Library].[uspInsertLibraryBook] 'A Game of Thrones', 'Penguin Random House';
+EXEC [Library].[uspInsertLibraryBook] 'An Elegant Universe', 'W. W. Norton Company';
+EXEC [Library].[uspInsertLibraryBook] 'C Programming: A Modern Approach', 'W. W. Norton Company';
+EXEC [Library].[uspInsertLibraryBook] 'A Clockwork Orange', 'William Heinemann';
+EXEC [Library].[uspInsertLibraryBook] 'On The Road', 'Viking Press';
+EXEC [Library].[uspInsertLibraryBook] 'Deadeye Dick', 'Dell Publishing';
+EXEC [Library].[uspInsertLibraryBook] 'Fear and Loathing in Las Vegas', 'Random House';
+EXEC [Library].[uspInsertLibraryBook] 'The Great Gatsby', 'Charles Scribner''s Sons';
+EXEC [Library].[uspInsertLibraryBook] 'The Fountainhead', 'Bobbs-Merrill Company';
+EXEC [Library].[uspInsertLibraryBook] 'Nineteen Eighty-Four', 'Harvill Secker';
+EXEC [Library].[uspInsertLibraryBook] 'Fight Club', 'W. W. Norton Company';
+
+
+
+-- Insert records into Library.BookAuthors --
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Lost Tribe', 'Mark Lee';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The 7 Habits of Highly Effective People', 'Steven R. Covey';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Power of Now: A Guide to Spiritual Enlightenment', 'Eckhart Tolle';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Ready Player One', 'Ernest Cline';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Alchemist', 'Paulo Coelho';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Family', 'Mario Puzo';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Mastery', 'George Leonard';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Infinite Jest', 'David Foster Wallace';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Ender''s Game', 'Orson Scott Card';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'A Game of Thrones', 'George R. R. Martin'; 
+EXEC [Library].[uspInsertLibraryBookAuthors] 'An Elegant Universe', 'Brian Greene'; 
+EXEC [Library].[uspInsertLibraryBookAuthors] 'C Programming: A Modern Approach', 'K. N. King';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'A Clockwork Orange', 'Anthony Burgess';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'On The Road', 'Jack Kerouac';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Deadeye Dick', 'Kurt Vonnegut';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Fear and Loathing in Las Vegas', 'Hunter S. Thompson';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Great Gatsby', 'F. Scott Fitzgerald';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'The Fountainhead', 'Ayn Rand';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Nineteen Eighty-Four', 'George Orwell';
+EXEC [Library].[uspInsertLibraryBookAuthors] 'Fight Club', 'Chuck Palahniuk';
